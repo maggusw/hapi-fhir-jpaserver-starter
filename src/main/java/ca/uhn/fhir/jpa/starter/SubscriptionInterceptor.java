c5328266aef865dbc26d9c21f88649af903e9f04
Markus Wieczorek <markus.wieczorek@healex.systems>
2022-01-21 10:09:26 +0100

    fix: Fix logging of specific resource type

--- src/main/java/ca/uhn/fhir/jpa/starter/SubscriptionInterceptor.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/SubscriptionInterceptor.java
@@ -5,6 +5,7 @@ import ca.uhn.fhir.interceptor.api.Interceptor;
 import ca.uhn.fhir.interceptor.api.Pointcut;
 import ca.uhn.fhir.rest.api.Constants;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
+import org.hl7.fhir.r4.model.Patient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,10 +17,11 @@ public class SubscriptionInterceptor {
 
 	@Hook(Pointcut.STORAGE_PRESTORAGE_RESOURCE_CREATED)
 	public void logIncomingResource(RequestDetails theRequest) {
-
 		byte[] requestContents = theRequest.loadRequestContents();
-		String contents = new String(requestContents, Constants.CHARSET_UTF8);
-		logger.info("Subscription interceptor received resource");
-		logger.info(contents);
+		if (theRequest.getResource().getClass() == Patient.class) {
+			String contents = new String(requestContents, Constants.CHARSET_UTF8);
+			logger.info("Subscription interceptor received resource");
+			logger.info(contents);
+		}
 	}
 }
